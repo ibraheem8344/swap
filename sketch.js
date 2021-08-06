a737bb73e8e25ffe9b3bc9893fb4dda9ff53da0b
ibraheem8344 <88517866+ibraheem8344@users.noreply.github.com>
2021-08-05 20:55:53 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+
+var a = prompt('Enter the first variable: ');
+
+function setup() {
+  var b2 = createButton("click here to Swap");
+
+  }
+
+
+function draw()
+{
+}
+function swap{} {
+  [a,b b,a]
+}
+console.log("value of a is"+a)
+console.log("value of b is"+b)
